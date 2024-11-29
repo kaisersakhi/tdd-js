7dac242f200199936766538a00c8cea3505fa9ce
Kaiser Sakhi <mail@kaisersakhi.com>
2024-11-29 19:17:09 +0530

    Refactor: test case assertion text

--- main.test.js
+++ main.test.js
@@ -2,7 +2,7 @@ const add = require('./main');
 
 describe("String Calculator", () => {
   describe("when the string is empty", () =>
-      it("adds numbers passed as comma separated numbers", () => {
+      it("returns zero", () => {
         expect(add("")).toBe(0);
       })
   );

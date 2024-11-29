1afbd8268cc83edcb8358d9591d4625bf1567afd
Kaiser Sakhi <mail@kaisersakhi.com>
2024-11-29 19:29:47 +0530

    Return sum of numbers passed as CSV to add

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-function add(numbers) {
-  if (numbers === undefined || numbers.length === 0) {
+function add(stringNumbers) {
+  if (stringNumbers === undefined || stringNumbers.length === 0) {
     return 0;
   }
+  
+  let numbers = stringArrayToNumbers(stringNumbers);
+  
+  return numbers.reduce((acc, number) => {
+    return acc + number;
+  })
+}
+
+function stringArrayToNumbers(stringNumbers) {
+  return stringNumbers
+      .split(",")
+      .map((strNumber) => {
+        return parseInt(strNumber);
+      })
 }
 
 module.exports = add;

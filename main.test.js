fa21b0cfd17d28418bb867b8a7baa5846030a545
Kaiser Sakhi <mail@kaisersakhi.com>
2024-11-29 19:33:11 +0530

    Add test case for multiple numbers

--- main.test.js
+++ main.test.js
@@ -11,5 +11,11 @@ describe("String Calculator", () => {
     it("return the same number", () => {
       expect(add("1")).toBe(1);
     })
+  });
+  
+  describe("when the string contains ten numbers", () => {
+    it("returns the sum of the numbers", () => {
+      expect(add("1, 2, 3, 4, 5, 6, 7, 8, 9, 10")).toBe(55);
+    })
   })
 })
